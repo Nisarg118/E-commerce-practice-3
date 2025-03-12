2bbfdfba5909558db272238a8456a43347ef939a
Nisarg118 <nisargbhamat12@gmail.com>
2025-03-12 23:53:34 +0530

    1-Header

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import UserLayout from "./components/Layout/UserLayout";
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<UserLayout />} />
+        <Route>{/*Admin Layout*/}</Route>
+      </Routes>
+    </>
+  );
+};
+
+export default App;
