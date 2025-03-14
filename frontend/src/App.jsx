cc1ca5968787b448503280f8fa470c3377e84322
Nisarg118 <nisargbhamat12@gmail.com>
2025-03-14 23:27:57 +0530

    2-HomePage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import UserLayout from "./components/Layout/UserLayout";
+import Home from "./pages/Home";
+import { Toaster } from "sonner";
 
 const App = () => {
   return (
     <>
+      <Toaster position="top-right" />
       <Routes>
-        <Route path="/" element={<UserLayout />} />
+        <Route path="/" element={<UserLayout />}>
+          <Route index element={<Home />} />
+        </Route>
         <Route>{/*Admin Layout*/}</Route>
       </Routes>
     </>
--- frontend/src/components/Layout/UserLayout.jsx
+++ frontend/src/components/Layout/UserLayout.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import Header from "../Common/Header";
+import Footer from "../Common/Footer";
+import { Outlet } from "react-router-dom";
 
 const UserLayout = () => {
   return (
     <div>
       <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
     </div>
   );
 };
